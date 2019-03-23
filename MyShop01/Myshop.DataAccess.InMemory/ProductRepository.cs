b94601f3776e8ce97ca4519cf443cc3e064b1dc9
ShiangTing <treetwood@gmail.com>
2019-03-24 01:06:37 +0800

    Product Manager Pages Built and Test

--- MyShop01/Myshop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop01/Myshop.DataAccess.InMemory/ProductRepository.cs
@@ -11,13 +11,13 @@ namespace Myshop.DataAccess.InMemory
     public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
-        List<Product> products = new List<Product>();
+        List<Product> products ;
         public ProductRepository()
         {
             products = cache["products"] as List<Product>;
             if (products == null)
             {
-                List<Product> products = new List<Product>();
+                 products = new List<Product>();
             }
 
         }
