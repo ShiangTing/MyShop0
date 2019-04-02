4732ec8307a14cafc8bcbc689932e94b5107318e
ShiangTing <treetwood@gmail.com>
2019-04-02 17:19:44 +0800

    SQL

--- MyShop01/Myshop.WebUI.Test/App_Start/UnityConfig.cs
+++ MyShop01/Myshop.WebUI.Test/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using Myshop.Core.Contracts;
 using Myshop.Core.Models;
 using Myshop.DataAccess.InMemory;
+using Myshop.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace Myshop.WebUI.Test
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
