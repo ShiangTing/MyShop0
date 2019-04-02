f43a2684ffd051e8ac2e821a93a54cd4cdf38bd3
ShiangTing <treetwood@gmail.com>
2019-04-03 01:31:56 +0800

    產品頁面

--- MyShop01/Myshop.WebUI.Test/Controllers/HomeController.cs
+++ MyShop01/Myshop.WebUI.Test/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Myshop.Core.Contracts;
+using Myshop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,33 @@ namespace Myshop.WebUI.Test.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
+        IRepository<Product> context; //Load product from the database
+        IRepository<ProductCategory> productCategories;//Load productCategories from the database
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        { // create a new instance(initialize)
+          // Concreat Implementation of IRepository
+            context = productContext;
+            productCategories = productCategoryContext;
         }
+        public ActionResult Index()
+        { // 增加product list
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (Id == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
 
         public ActionResult About()
         {
